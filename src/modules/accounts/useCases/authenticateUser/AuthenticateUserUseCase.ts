bcc54a7aaf66fbe26e95789edc7598f4b9ae5246
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-07-12 22:55:28 -0300

    feat: Implement functionality to create a session.

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,52 @@
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
+import { inject, injectable } from "tsyringe"
+
+import { IUsersRepository } from "../../repositories/IUsersRepository"
+
+interface IRequest {
+  email: string
+  password: string
+}
+
+interface IResponse {
+  user: {
+    name: string
+    email: string
+  },
+  token: string
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) { }
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new Error("Email or password invalid!")
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new Error("Email or password invalid!")
+    }
+
+    const token = sign({}, process.env.JWT_SECRET, {
+      subject: user.id,
+      expiresIn: "1d"
+    })
+
+    return {
+      user: {
+        name: user.email,
+        email: user.email
+      },
+      token
+    }
+  }
+}
