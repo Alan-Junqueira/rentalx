d4c06c65882441109254257f963e71e1eef27477
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-07-14 18:42:34 -0300

    test: Implement test to validate category creation.

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,6 +1,41 @@
-describe("Create Category", () => {
-  it("Should create a category", () => {
+import { AppError } from "../../../../errors/AppError"
+import { InMemoryCategoriesRepository } from "../../repositories/in-memory/InMemoryCategoriesRepository"
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
 
-    expect(2+2).toBe(4)
+let imMemoryCategoriesRepository: InMemoryCategoriesRepository
+let sut: CreateCategoryUseCase
+
+describe("Create Category", () => {
+  beforeEach(() => {
+    imMemoryCategoriesRepository = new InMemoryCategoriesRepository()
+    sut = new CreateCategoryUseCase(imMemoryCategoriesRepository)
+  })
+
+  it("Should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category description Test"
+    }
+
+    await sut.execute(category)
+
+    const createdCategory = await imMemoryCategoriesRepository.findByName(category.name)
+
+    expect(createdCategory).toHaveProperty("id")
+  })
+
+  it("Should not be able to create a new category with existent name.", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category description Test"
+    }
+
+    expect(async () => {
+      await sut.execute(category)
+      await sut.execute(category)
+
+      await imMemoryCategoriesRepository.findByName(category.name)
+    }).rejects.toBeInstanceOf(AppError)
+
   })
 })
