5f49c1d7da5797626660bc35ef85bcc6cb464f67
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-07-15 01:34:05 -0300

    feat: Create authentication tests.

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,66 @@
+import dotenv from "dotenv"
+
+import { AppError } from "../../../../errors/AppError"
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO"
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository"
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase"
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
+
+dotenv.config()
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let createUserUseCase: CreateUserUseCase
+let sut: AuthenticateUserUseCase
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository)
+    sut = new AuthenticateUserUseCase(inMemoryUsersRepository)
+  })
+
+  it("Should be able to authenticate a user", async () => {
+    const user: ICreateUserDTO = {
+      driverLicense: "00123",
+      email: "teste@email.com",
+      name: "User",
+      password: "1234"
+    }
+
+    await createUserUseCase.execute(user)
+
+    const result = await sut.execute({
+      email: user.email,
+      password: user.password
+    })
+
+    expect(result).toHaveProperty("token")
+  })
+
+  it("Should not be able to authenticate an inexistent user", () => {
+    expect(async () => {
+      await sut.execute({
+        email: "false@email.com",
+        password: "false"
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+
+  it("Should not be able to authenticate with incorrect password", () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        driverLicense: "00123",
+        email: "teste@email.com",
+        name: "User",
+        password: "correct"
+      }
+
+      await createUserUseCase.execute(user)
+
+      await sut.execute({
+        email: user.email,
+        password: "wrong"
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+})
