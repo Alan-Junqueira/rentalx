3f1473816d1252ae42a9c8961e2cead4e953c7c7
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-08-02 00:13:43 -0300

    feat: Impleent refresh token controller.

--- src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { RefreshTokenUseCase } from "./RefreshTokenUseCase";
 
 export class RefreshTokenController {
-  handle(req: Request, res: Response): Promise<Response> {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const token = req.body.token || req.headers["x-access-token"] || req.query.token
 
+    const refreshTokenUseCase = container.resolve(RefreshTokenUseCase)
+
+    const refreshToken = await refreshTokenUseCase.execute(token)
+
+    return res.json(refreshToken)
   }
 }
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express"
 import { verify } from "jsonwebtoken"
 
 import { authConfig } from "@config/auth"
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository"
+import { UserTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UserTokensRepository"
 import { AppError } from "@shared/errors/AppError"
 
 
@@ -13,6 +13,8 @@ interface IPayload {
 export const ensureAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization
 
+  const userTokensRepository = new UserTokensRepository()
+
   if (!authHeader) {
     throw new AppError("Token missing", 401)
   }
@@ -20,10 +22,9 @@ export const ensureAuthenticated = async (req: Request, res: Response, next: Nex
   const [, token] = authHeader.split(" ")
 
   try {
-    const { sub } = verify(token, authConfig.secretToken) as IPayload
+    const { sub } = verify(token, authConfig.secretRefreshToken) as IPayload
 
-    const usersRepository = new UsersRepository()
-    const user = await usersRepository.findById(sub)
+    const user = await userTokensRepository.findByUserIdAndRefreshToken({ refreshToken: token, userId: sub })
 
     if (!user) {
       throw new AppError("User does not exists", 401)
