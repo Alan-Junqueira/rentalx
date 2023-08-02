8d749bbd12e2bcd782ce96056254a6230d0dd30e
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-08-02 16:08:07 -0300

    feat: Upload image using bucket s3.

---
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,46 @@
+import { S3 } from 'aws-sdk'
+import fs from 'fs'
+import mime from 'mime'
+import path from 'path'
+
+import { uploadConfig } from '@config/upload';
+
+import { IStorageProvider, IStorageProviderDelete, IStorageProviderSave } from "../IStorageProvider";
+
+export class S3StorageProvider implements IStorageProvider {
+  private client: S3
+
+  constructor() {
+    this.client = new S3({
+      region: process.env.AWS_BUCKET_REGION
+    })
+  }
+
+  async delete({ file, folder }: IStorageProviderDelete): Promise<void> {
+    await this.client.deleteObject({
+      Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+      Key: file,
+    }).promise()
+  }
+
+  async save({ file, folder }: IStorageProviderSave): Promise<string> {
+    const originalName = path.resolve(uploadConfig.tmpFolder, file)
+
+    const fileContent = await fs.promises.readFile(originalName)
+
+    const ContentType = mime.getType(originalName)
+
+    await this.client.putObject({
+      Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+      Key: file,
+      // ACL: "public-read", // ? "private"|"public-read"|"public-read-write"|"authenticated-read"|"aws-exec-read"|"bucket-owner-read"|"bucket-owner-full-control"|string
+      Body: fileContent,
+      ContentType
+    }).promise()
+
+    await fs.promises.unlink(originalName)
+
+    return file
+  }
+
+}
